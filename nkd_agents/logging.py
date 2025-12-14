85ed98e063dbe778db14efddc07a3d5005db8abb
Amitej Mehta <amitejmehta@berkeley.edu>
2025-12-13 20:38:05 -0500

    refactor(logging): minor logging color constant tweaks

--- nkd_agents/logging.py
+++ nkd_agents/logging.py
@@ -3,10 +3,12 @@ import logging
 import sys
 
 IS_TTY = sys.stderr.isatty()
-logging_context = contextvars.ContextVar("logging_context", default={})
+GREEN = "\033[32m" if IS_TTY else ""
+RED = "\033[31m" if IS_TTY else ""
+RESET = "\033[0m" if IS_TTY else ""
+DIM = "\033[38;5;242m" if IS_TTY else ""
 
-GREEN_TTY = "\033[32m" if IS_TTY else ""
-RESET_TTY = "\033[0m" if IS_TTY else ""
+logging_context = contextvars.ContextVar("logging_context", default={})
 
 
 class ContextFilter(logging.Filter):
@@ -20,10 +22,9 @@ def configure_logging() -> None:
     if IS_TTY:
         # ANSI codes: \033[1m = bold, \033[2m = dim, \033[38;5;N = 256 color, \033[0m = reset
         fmt = "\n\033[38;5;65m%(asctime)s\033[38;5;102m | %(levelname)s\033[0m\033[38;5;103m | %(name)s:%(funcName)s:%(lineno)s\033[38;5;255m - %(message)s\033[0m%(context)s"
-        logging.getLogger("httpx").setLevel(logging.WARNING)
-
     else:
         fmt = "%(asctime)s | %(levelname)s | %(name)s:%(funcName)s:%(lineno)s - %(message)s%(context)s"
     handler = logging.StreamHandler(sys.stderr)
     handler.addFilter(ContextFilter())
     logging.basicConfig(level=logging.INFO, format=fmt, handlers=[handler], force=True)
+    logging.getLogger("httpx").setLevel(logging.WARNING)
